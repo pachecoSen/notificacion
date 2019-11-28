b522135be50d754b9abe6a411116f2fc8ad45a0f
pachecoSen <pachecodr2013@gmail.com>
2019-11-28 14:36:33 -0600

    migra

--- app/src/main/java/classes/HTTP.java
+++ app/src/main/java/classes/HTTP.java
@@ -1,22 +1,89 @@
 package classes;
 
-public class HTTP {
-    public String host;
+import android.content.Context;
+import android.util.Log;
 
-    public HTTP(String host){
-        this.host=host;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class HTTP {
+    private static final String TAG;
+
+    static {
+        TAG = "CLASS - HTTP";
     }
 
-    public String getHost() {
+    private String host;
+    private String method;
+    private RequestQueue queue;
+    private String ruta;
+
+    public HTTP(String host){
+        this.setHost(host);
+        this.setMethod("get");
+    }
+
+    private String getHost() {
         return host;
     }
 
-    public void setHost(String host) {
+    private void setHost(String host) {
+        host = host.toLowerCase();
+        Log.d(TAG, "setHost: " + host);
         this.host = host;
     }
 
-    public HTTP host(String host){
-        this.setHost(host);
+    private String getMethod() {
+        return method;
+    }
+
+    private void setMethod(String method) {
+        method = method.toLowerCase();
+        Log.d(TAG, "setMethod: " + method);
+        this.method = method;
+    }
+
+    private String getRuta() {
+        return ruta;
+    }
+
+    private void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public void requerido(Context contexto){
+        this.queue = Volley.newRequestQueue(contexto);
+        final int metodo = "GET".equals(this.getMethod()) ? Request.Method.GET : Request.Method.POST;
+        String ruta = this.getHost() + this.getRuta();
+        ruta = ruta.replace("//", "/");
+        Log.d(TAG, "requerido: " + "Route: " + ruta);
+        StringRequest stringRequest = new StringRequest(metodo, ruta,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.d(TAG, "onResponse: " + response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d(TAG, "onErrorResponse: " + error.getMessage());
+                }
+            }
+        );
+    }
+
+    public HTTP deffMethod(String method){
+        this.setMethod(method);
+
+        return this;
+    }
+
+    public HTTP deffRuta(String ruta){
+        this.setRuta(ruta);
 
         return this;
     }
--- app/src/main/java/com/example/notificacion/MainActivity.java
+++ app/src/main/java/com/example/notificacion/MainActivity.java
@@ -1,11 +1,24 @@
 package com.example.notificacion;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.util.Log;
+import classes.HTTP;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG;
+
+    static {
+        TAG = "[MAIN]";
+    }
+
+    private HTTP http;
+
+    public MainActivity(){
+        http = new HTTP("http://192.168.1.43/");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +27,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i("Resume", "APP Resume");
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "APP Iniciada...", Toast.LENGTH_SHORT).show();
+        this.http.deffMethod("GET").deffRuta("/ticket/latest").requerido(getApplicationContext());
     }
 }
